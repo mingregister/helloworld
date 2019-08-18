6ff76b12037213ab74df6caab5861304d2bc2bbd
mingregister <774262573@qq.com>
2019-08-19 01:43:09 +0800

    admin页面下已可以实现自动选择用户

--- helloworld/blog/admin.py
+++ helloworld/blog/admin.py
@@ -1,14 +1,16 @@
 from django.contrib import admin
 
+from .models import Blog
+
 # Register your models here.
 
-# 1.你可以通过重写ModelAdmin的get_form方法来做到这一点。
-
-# 2.now, the blow way can not be work.
-class BlogModelAdmin(admin.ModelAdmin):
+# 这个方法可以实现选择blogger时，自动选择当前用户。
+# 但是：仅限于在admin页面下.
+@admin.register(Blog)
+class BlogAdmin(admin.ModelAdmin):
     def formfield_for_foreignkey(self, db_field, request, **kwargs):
         if db_field.name == 'blogger':
             kwargs['initial'] = request.user.id
-        return super(BlogModelAdmin, self).formfield_for_foreignkey(
-            db_field, request, **kwargs
-        )
+        return super(BlogAdmin, self).formfield_for_foreignkey(
+                    db_field, request, **kwargs)
+
