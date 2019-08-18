d532f09dd2504790f8833a0453e47c291d6cae30
mingregister <774262573@qq.com>
2019-08-19 00:07:16 +0800

    chioce post bloger

--- helloworld/blog/models.py
+++ helloworld/blog/models.py
@@ -1,9 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
+
 class Blog(models.Model):
-    blogger = models.CharField(max_length=50)
+    # blogger = models.CharField(max_length=50)
+    # # https://docs.djangoproject.com/en/2.2/ref/models/fields/#django.db.models.ForeignKey.related_name
+    blogger = models.ForeignKey(User, on_delete=models.CASCADE, related_name='+')
     title = models.TextField(max_length=200, blank=False, null=False)
     create_time = models.DateTimeField(auto_now=False, auto_now_add=True)
     modified_time = models.DateTimeField(auto_now=True, auto_now_add=False)
@@ -17,3 +21,22 @@ class Blog(models.Model):
         db_table = 'blog'
         ordering = ['-modified_time']
         get_latest_by =  ['-modified_time', '-create_time']
+
+
+# class golb(models.Model):
+#     # blogger = models.CharField(max_length=50)
+#     # # https://docs.djangoproject.com/en/2.2/ref/models/fields/#django.db.models.ForeignKey.related_name
+#     blogger = models.ForeignKey(User, on_delete=models.CASCADE, related_name='+')
+#     title = models.TextField(max_length=200, blank=False, null=False)
+#     create_time = models.DateTimeField(auto_now=False, auto_now_add=True)
+#     modified_time = models.DateTimeField(auto_now=True, auto_now_add=False)
+#     body = models.TextField()
+# 
+# 
+#     def __str__(self):
+#         return ": ".join([self.blogger, self.title])
+# 
+#     class Meta:
+#         db_table = 'golb'
+#         ordering = ['-modified_time']
+#         get_latest_by =  ['-modified_time', '-create_time']
--- helloworld/demo/models.py
+++ helloworld/demo/models.py
@@ -17,11 +17,14 @@ class Board(models.Model):
 class Topic(models.Model):
     subject = models.CharField(max_length=255)
     last_updated = models.DateTimeField(auto_now_add=True)
+    # https://docs.djangoproject.com/en/2.2/ref/models/fields/#django.db.models.ForeignKey.related_name
+    # 一对多关系[一个Board对应0-*个Topic]，外键定义在*多(Topic)*的一方。
     board = models.ForeignKey(Board, on_delete=models.CASCADE, related_name='topics')
     starter = models.ForeignKey(User, on_delete=models.CASCADE, related_name='topics')
 
 class Post(models.Model):
     message = models.TextField(max_length=4000)
+    # 一对多关系[一个Topic对应1-*个Post]，外键定在*多(Post)*的一方
     topic = models.ForeignKey(Topic, on_delete=models.CASCADE, related_name='posts')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(null=True)
